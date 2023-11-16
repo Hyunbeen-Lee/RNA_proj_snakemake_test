337095f6e14ad80f3dee5304e408481473983460
Hyunbeen-Lee <150957884+Hyunbeen-Lee@users.noreply.github.com>
2023-11-16 05:22:17 -0500

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,9 +1,9 @@
 """Snakemake wrapper for Infernal CMscan"""
 
-__author__ = "N. Tessa Pierce"
-__copyright__ = "Copyright 2019, N. Tessa Pierce"
-__email__ = "ntpierce@gmail.com"
-__license__ = "MIT"
+#__author__ = "N. Tessa Pierce"
+#__copyright__ = "Copyright 2019, N. Tessa Pierce"
+#__email__ = "ntpierce@gmail.com"
+#__license__ = "MIT"
 
 from os import path
 from snakemake.shell import shell
